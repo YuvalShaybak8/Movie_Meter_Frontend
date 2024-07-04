e5b001e6c76ba0fef7ea808784761e3da4856e6d
yuvals <yuvalsaybak@cs.colman.ac.il>
2024-07-05 00:35:20 +0300

    add a trailer icon , still not working fine

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../components/Layout";
 import "../styles/HomePage.css";
 import { movies } from "../data/movieDatabase.js";
+import trailerIcon from "../assets/trailer_icon.png";
+import axios from "axios";
+import Modal from "react-modal";
+
+const API_KEY = "b04e7ba5db91cb63b41243b69801c5f1";
 
 const HomePage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [videoUrl, setVideoUrl] = useState("");
+
+  const fetchTrailer = async (movieId) => {
+    try {
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${API_KEY}`
+      );
+      const trailer = response.data.results.find(
+        (video) => video.type === "Trailer"
+      );
+      if (trailer) {
+        setVideoUrl(`https://www.youtube.com/watch?v=${trailer.key}`);
+        setIsOpen(true);
+      } else {
+        alert("Trailer not available");
+      }
+    } catch (error) {
+      console.error("Error fetching trailer:", error);
+      alert("Failed to fetch trailer");
+    }
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+    setVideoUrl("");
+  };
+
   return (
     <Layout>
       <div className="home-page">
@@ -23,7 +56,11 @@ const HomePage = () => {
                   </div>
                   <div className="movie-info">
                     <div className="movie-title">{movie.title}</div>
-                    <div className="movie-year">{movie.year}</div>
+                    <img
+                      src={trailerIcon}
+                      alt="Trailer"
+                      className="trailer-icon"
+                    />
                   </div>
                 </div>
               ))}
