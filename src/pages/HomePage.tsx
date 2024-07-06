a36033688284f69b9bbe317e42083d0b93c9c53e
yuvals <yuvalsaybak@cs.colman.ac.il>
2024-07-06 19:42:06 +0300

    create my rathing for a movie

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,12 +6,22 @@ import Layout from "../components/Layout";
 import { movies } from "../data/movieDatabase.js";
 import trailerIcon from "../assets/trailer_icon.png";
 import commentIcon from "../assets/comment_icon.png";
+import ratingIn from "../assets/rating_in.png";
+import ratingOut from "../assets/rating_out.png";
+import myRating from "../assets/my_rating.png";
+import { FaStar } from "react-icons/fa";
 
 const API_KEY = "b04e7ba5db91cb63b41243b69801c5f1";
 
 const HomePage = () => {
   const [videoUrl, setVideoUrl] = useState<string | null>(null);
   const [isOpen, setIsOpen] = useState(false);
+  const [ratingOpen, setRatingOpen] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+  const [userRating, setUserRating] = useState(0);
+  const [ratings, setRatings] = useState({});
 
   const fetchTrailer = async (movieTitle: string) => {
     try {
@@ -43,6 +53,19 @@ const HomePage = () => {
     }
   };
 
+  const handleRatingClick = (movie) => {
+    setSelectedMovie(movie);
+    setRatingOpen(true);
+  };
+
+  const handleRateMovie = () => {
+    setRatings((prevRatings) => ({
+      ...prevRatings,
+      [selectedMovie.id]: userRating,
+    }));
+    setRatingOpen(false);
+  };
+
   return (
     <Layout>
       <div className="home-page">
@@ -58,17 +81,31 @@ const HomePage = () => {
                 <div key={movie.id} className="movie-card">
                   <div className="movie-poster">
                     <img src={movie.image} alt={movie.title} />
-                    <div className="movie-rating">{movie.rating}</div>
+                    <div className="movie-rating">
+                      <img
+                        src={ratingIn}
+                        alt="Rating"
+                        className="rating-icon"
+                      />
+                      <span className="rating-text">{movie.rating}</span>
+                    </div>
+                    <div className="movie-rating-top-left">
+                      <img
+                        src={ratings[movie.id] ? myRating : ratingOut}
+                        alt="Rating"
+                        className="rating-icon"
+                        onClick={() => handleRatingClick(movie)}
+                      />
+                      {ratings[movie.id] && (
+                        <span className="my-rating-text">
+                          {ratings[movie.id]}
+                        </span>
+                      )}
+                    </div>
                   </div>
                   <div className="movie-info">
                     <div className="movie-title">{movie.title}</div>
                     <div className="movie-icons">
-                      <img
-                        src={trailerIcon}
-                        alt="Trailer"
-                        className="trailer-icon"
-                        onClick={() => fetchTrailer(movie.title)}
-                      />
                       <Link to={`/comments/${movie.id}`}>
                         <img
                           src={commentIcon}
@@ -76,6 +113,17 @@ const HomePage = () => {
                           className="comment-icon"
                         />
                       </Link>
+                      <button
+                        className="trailer-button"
+                        onClick={() => fetchTrailer(movie.title)}
+                      >
+                        <img
+                          src={trailerIcon}
+                          alt="Trailer"
+                          className="trailer-icon"
+                        />
+                        <span className="trailer-text">Trailer</span>
+                      </button>
                     </div>
                   </div>
                 </div>
@@ -99,6 +147,47 @@ const HomePage = () => {
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
               ></iframe>
+            </div>
+          </div>
+        )}
+
+        {ratingOpen && (
+          <div className="modal-rating" onClick={() => setRatingOpen(false)}>
+            <div
+              className="modal-content-rating"
+              onClick={(e) => e.stopPropagation()}
+            >
+              <span className="close" onClick={() => setRatingOpen(false)}>
+                &times;
+              </span>
+              <h2>Rate This Movie</h2>
+              <h3>{selectedMovie?.title}</h3>
+              <div className="star-my-rating">
+                {[...Array(10)].map((star, index) => {
+                  const ratingValue = index + 1;
+                  return (
+                    <label key={index}>
+                      <input
+                        type="radio"
+                        name="rating"
+                        value={ratingValue}
+                        onClick={() => setUserRating(ratingValue)}
+                        style={{ display: "none" }}
+                      />
+                      <FaStar
+                        className={`star ${
+                          ratingValue <= (hover || userRating) ? "active" : ""
+                        }`}
+                        onMouseEnter={() => setHover(ratingValue)}
+                        onMouseLeave={() => setHover(0)}
+                      />
+                    </label>
+                  );
+                })}
+              </div>
+              <button className="rate-button" onClick={handleRateMovie}>
+                Rate
+              </button>
             </div>
           </div>
         )}
