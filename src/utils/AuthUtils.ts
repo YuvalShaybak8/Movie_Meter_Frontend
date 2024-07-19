b96aa3ddd21acc54d66287b27d3aad68c077b588
yuvals <yuvalsaybak@cs.colman.ac.il>
2024-07-19 16:07:36 +0300

    delete files that we dont need

--- src/utils/AuthUtils.ts
+++
@@ -1,6 +0,0 @@
-export const logout = () => {
-  // Clear user session (implement as per your authentication logic)
-  console.log("User logged out");
-  // Redirect to login page
-  window.location.href = "/";
-};
