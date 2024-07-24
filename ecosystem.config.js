3fd6f6c59681ae9c5ee92f259b7751a66a03a1dd
yuvals <yuvalsaybak@cs.colman.ac.il>
2024-07-24 15:25:45 +0300

    fix some issues

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   apps: [
     {
-      name: "client", // Name of your application
-      script: "./App.tsx", // Entry point of your application
+      name: "client",
+      script: "./src/App.tsx",
       env_production: {
-        VITE_REACT_APP_API_URL: "http://localhost:80",
+        VITE_REACT_APP_API_URL: "https://node09.cs.colman.ac.il",
         VITE_API_KEY: "b04e7ba5db91cb63b41243b69801c5f1",
         VITE_GOOGLE_CLIENT_ID:
           "403571040301 - 2ka5e09b8upflfk8qmpfebo4sg08gde0.apps.googleusercontent.com",
